293d33943ce75f0e0fcde5165ad6fad98b2e7e14
Matt Powell <fauxparse@gmail.com>
2010-07-22 16:28:33 +1200

    start comment admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,4 @@
 Rails.application.routes.draw do |map|
-  
-  # scope "admin", :name_prefix => "admin", :module => "breeze/blog" do
-  #   resources :blog do
-  #     collection do
-  #       post :setup_default
-  #       put :switch
-  #       get :settings
-  #       put :settings
-  #     end
-  #   end
-  #   scope "blog", :name_prefix => "blog" do
-  #     resources :posts
-  #   end
-  #   # resources :posts
-  # end
-
   scope "admin/blog", :name_prefix => "blog", :module => "breeze/blog" do
     root :to => "blogs#index"
     controller :blogs do
@@ -23,7 +7,8 @@ Rails.application.routes.draw do |map|
       get :settings
       put :settings
     end
+    
     resources :posts, :name_prefix => "admin"
+    resources :comments, :name_prefix => "admin"
   end
-  
 end
