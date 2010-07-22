8a0b882afd8fa1a510a1eb395e954667010a49f7
Chris Bartlett <chris@leftclick.com>
2010-07-22 12:01:09 +1200

    Added tabs for post status on blog index view

--- app/models/breeze/blog/post.rb
+++ app/models/breeze/blog/post.rb
@@ -12,9 +12,28 @@ module Breeze
       field :title
       field :slug
       field :body
+      field :intro
+      field :published_at, :type => Time
       
       validates_presence_of :title, :slug, :body
       validates_presence_of :author_id, :message => "must be selected"
+      
+      scope :published, lambda { where(:published_at.lte => Time.now.utc) }
+      scope :pending,   lambda { where(:published_at.gt => Time.now.utc) }
+      scope :draft,     lambda { where(:published_at => nil) }
+
+      def published?
+        published_at.present? && published_at <= DateTime.now
+      end
+           
+      def pending?
+        published_at.present? && published_at > DateTime.now
+      end
+      
+      def status
+        published? ? :published : :draft
+      end
+
     end
   end
 end
