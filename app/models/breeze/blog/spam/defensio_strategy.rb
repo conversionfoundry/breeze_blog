f26c9c3d890da6ea58dd1ab6300b94097f682883
Matt Powell <fauxparse@gmail.com>
2010-07-27 13:02:41 +1200

    only load Defensio strategy if Defender is loaded

--- app/models/breeze/blog/spam/defensio_strategy.rb
+++ app/models/breeze/blog/spam/defensio_strategy.rb
@@ -1,35 +1,37 @@
-module Breeze
-  module Blog
-    module Spam
-      class DefensioStrategy < Strategy
-        field :api_key
+if defined?(Defender)
+  module Breeze
+    module Blog
+      module Spam
+        class DefensioStrategy < Strategy
+          field :api_key
       
-        def submit(comment)
-          Defender.api_key = api_key
-          document = document_from(comment)
-          if document.save
-            if document.allow?
-              comment.publish! unless comment.blog.comment_moderation?
-            else
-              comment.spam!
+          def submit(comment)
+            Defender.api_key = api_key
+            document = document_from(comment)
+            if document.save
+              if document.allow?
+                comment.publish! unless comment.blog.comment_moderation?
+              else
+                comment.spam!
+              end
+              comment.update_attributes :defensio_signature => document.signature
             end
-            comment.update_attributes :defensio_signature => document.signature
           end
-        end
       
-        def document_from(comment)
-          if comment.attributes[:defensio_signature].present?
-            Defender::Document.find(comment.defensio_signature)
-          else
-            returning Defender::Document.new do |document|
-              document.data[:content]          = comment.body(:source)
-              document.data[:type]             = "comment"
-              document.data[:platform]         = "defender"
-              document.data[:author_name]      = comment.name
-              document.data[:author_email]     = comment.email
-              document.data[:author_url]       = comment.website
-              document.data[:author_logged_in] = comment.authenticated?
-              document.data[:author_trusted]   = comment.authenticated?
+          def document_from(comment)
+            if comment.attributes[:defensio_signature].present?
+              Defender::Document.find(comment.defensio_signature)
+            else
+              returning Defender::Document.new do |document|
+                document.data[:content]          = comment.body(:source)
+                document.data[:type]             = "comment"
+                document.data[:platform]         = "defender"
+                document.data[:author_name]      = comment.name
+                document.data[:author_email]     = comment.email
+                document.data[:author_url]       = comment.website
+                document.data[:author_logged_in] = comment.authenticated?
+                document.data[:author_trusted]   = comment.authenticated?
+              end
             end
           end
         end
