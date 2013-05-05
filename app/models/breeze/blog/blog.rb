4c9d26017109ef08b33cd46aac132b2671d6d7f2
Isaac Freeman <isaac@leftclick.com>
2013-05-06 10:12:36 +1200

    resolving merge

--- app/models/breeze/blog/blog.rb
+++ app/models/breeze/blog/blog.rb
@@ -2,7 +2,7 @@ module Breeze
   module Blog
     class Blog < Breeze::Content::Page
       extend ActiveSupport::Memoizable
-      
+
       field :posts_per_page, :type => Integer, :default => 5
       field :post_summary_length, :type => Integer, :default => 100
       field :comment_moderation, :type => Boolean, :default => false
@@ -13,26 +13,26 @@ module Breeze
       embeds_one :spam_strategy, :class_name => "Breeze::Blog::Spam::Strategy"
       field :rss_title
       field :rss_description
-      
+
       validates_associated :spam_strategy
-            
+
       before_create :create_default_views
       before_create :create_default_spam_filtering
       after_destroy :destroy_posts_and_categories
       before_save :fix_post_permalinks
-      
+
       def view_for(controller, request)
         if controller.admin_signed_in? && request.params[:view]
           returning views.by_name(request.params[:view]) do |view|
             view.with_url_params Breeze::Blog::PERMALINK.match(permalink)
           end
-        else  
+        else
           # permalink = request.path + request.params[:path]
           permalink = "/" + request.params[:path]
           view_from_permalink permalink
         end
       end
-      
+
       def view_from_permalink(permalink)
         view = case permalink
         when "/blog"
@@ -44,13 +44,9 @@ module Breeze
         else
           post_view
         end
-<<<<<<< HEAD
-
-=======
->>>>>>> 18f92ed3ba8175b60cbe561afdd15c4821a1a115
         view.with_url_params permalink
       end
-      
+
       def method_missing(sym, *args, &block)
         if sym.to_s =~ /^(.+)_view$/
           view_name = $1
@@ -60,7 +56,7 @@ module Breeze
           super
         end
       end
-      
+
       def self.find_by_permalink(permalink)
         post = Post.where(:permalink => permalink).first
         if post
@@ -72,18 +68,18 @@ module Breeze
           end
         end
       end
-      
+
       def spam_strategy_attributes=(attrs)
         if attrs[:_type]
           klass = attrs[:_type].constantize
           self.spam_strategy = klass.new(attrs.except(:id, :_type))
         end
       end
-      
+
       def rss_link
         permalink(:include_domain) + ".rss"
       end
-      
+
     protected
       def create_default_views
         index_view
@@ -91,16 +87,16 @@ module Breeze
         category_view
         post_view
       end
-      
+
       def create_default_spam_filtering
         self.spam_strategy = Breeze::Blog::Spam::NoStrategy.new
       end
-      
+
       def destroy_posts_and_categories
         posts.destroy_all
         categories.destroy_all
       end
-      
+
       def fix_post_permalinks
         if permalink_changed?
           # TODO: probably not the best idea to load them all at once
